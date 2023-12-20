4767951e86722de27d366c2ba1c28c9ee7ae73f5
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2023-12-20 14:16:53 +0530

    adding new file with new function

---
+++ Python3.py
@@ -0,0 +1,13 @@
+'''def calculate_rect_area(length, width):
+    area =length * width
+    return area
+
+area = calculate_rect_area(10,20)
+print(area)'''
+
+def add_number(a,b):
+    addition = a+b
+    return addition
+
+addition = add_number(5,20)
+print(addition)
--- python2.py
+++ python2.py
@@ -1,6 +1,8 @@
 def add_number(a,b):
-    return a+b
+    result = a+b
+    return result 
 
 result = add_number(10,25)
 
 print("the sum of two numbers", result)
+
