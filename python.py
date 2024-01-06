7a06ce355b5505b9529b187123b9e5f2163e1357
Varpe Jalindar <jalindarvarpe2015@gmail.com>
2024-01-06 18:49:18 +0530

    adding new changes

--- python.py
+++ python.py
@@ -1,9 +1,6 @@
-def calculate_rectangle_area(length, width):
+def caluculated_area(length, width):
     area = length * width
     return area
 
-# Calculate the area of a rectangle with length 5 and width 10
-area = calculate_rectangle_area(5, 10)
-print("The area of the rectangle is:", area)
-
-
+result = caluculated_area(10,15)
+print(result)
